65e251a5a3c9916d551654a1d697f35b097c1163
anselmobd <anselmo.blanco.dominguez@gmail.com>
2023-12-08 15:31:57 -0300

    vira carta, output e teste

--- scripts/baralho.py
+++ scripts/baralho.py
@@ -1,10 +1,16 @@
 from pprint import pprint
+from random import seed, randint
 
 
 class TipoBaralho:
 
+    TESTE = 'teste'
     ESPANHOL = 'espanhol'
     _NAIPES = {
+        TESTE:
+        {
+            'O': 'ouros',
+        },
         ESPANHOL:
         {
             'O': 'ouros',
@@ -14,6 +20,19 @@ class TipoBaralho:
         },
     }
     _CARTAS = {
+        TESTE:
+        {
+            '1': '1',
+            '2': '2',
+            '3': '3',
+            '4': '4',
+            '5': '5',
+            '6': '6',
+            '7': '7',
+            '8': '8',
+            '9': '9',
+            '10': '10',
+        },
         ESPANHOL:
         {
             'A': 'ás',
@@ -30,7 +49,7 @@ class TipoBaralho:
     }
 
     def __init__(self, tipo=None) -> None:
-        self.tipo = tipo if tipo else self.ESPANHOL
+        self.tipo = tipo if tipo else self.TESTE
 
     @property
     def naipes(self):
@@ -60,28 +79,61 @@ class Baralho:
 
     def inicializa_baralho(self):
         self.monte = [
-            (n, c)
+            (c, n)
             for n in self.tipo_baralho.naipes
             for c in self.tipo_baralho.cartas
         ]
+        self.descarte = []
     
-    def mostra_monte(self, n=0):
-        self.mostra(self.monte, n)
+    def vira_carta(self):
+        carta_idx = 0 if self.ordem == self.__ORDENADA else randint(0, len(self.monte)-1)
+        self.descarte.append(self.monte[carta_idx])
+        del(self.monte[carta_idx])
 
-    def mostra(self, cartas, n=0):
+    def mostra_monte(self, n=0):
+        self.mostra_cartas(self.monte, n)
+
+    def mostra_descarte(self, n=0):
+        self.mostra_cartas(self.descarte, n)
+
+    def mostra_tudo(self, n=0):
+        print('monte')
+        self.mostra_cartas(self.monte, n)
+        print('descarte')
+        self.mostra_cartas(self.descarte, n)
+
+    def get_cartas(self, cartas, n=0):
+        result = []
         for i, carta in enumerate(cartas, start=1):
-            pprint(carta)
+            result.append(carta)
             if i == n:
                 break
+        return result
+
+    def mostra_cartas(self, cartas, n=0):
+        for carta in self.get_cartas(cartas, n):
+            pprint(carta)
+
+    def str_tudo(self, n=0):
+        print('monte', self.str_cartas(self.monte, n))
+        print('descarte', self.str_cartas(self.descarte, n))
+
+    def str_cartas(self, cartas, n=0):
+        return "-".join(
+            [f"{c}{n}" for c, n in self.get_cartas(cartas, n)]
+        )
+
+
+def main():
+    baralho = Baralho()
+    baralho.embaralha()
+    
+    while baralho.monte:
+        baralho.str_tudo()
+        baralho.vira_carta()
+        baralho.str_tudo()
 
 
 if __name__ == '__main__':
-    baralho = Baralho()
-    baralho.mostra_monte(9)
-
-
-
-
-
-
-
+    seed(42)
+    main()
