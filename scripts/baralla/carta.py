f9722c45d3dd313838ca19b645ae5546b6e4fb6c
anselmobd <anselmo.blanco.dominguez@gmail.com>
2023-12-10 14:41:34 -0300

    iniciando class Carta

---
+++ scripts/baralla/carta.py
@@ -0,0 +1,14 @@
+from pprint import pprint
+from random import randint
+
+from baralla.tipo_baralho import TipoBaralho
+
+
+class Carta:
+
+    def __init__(self, numero, naipe) -> None:
+        self.numero = numero
+        self.naipe = naipe
+
+    def __str__(self) -> str:
+        return f"{self.numero}{self.naipe}"
