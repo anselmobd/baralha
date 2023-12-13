ef233da2e1d570874274a64ae77634bbfd093861
Anselmo Blanco Dominguez <anselmo.blanco.dominguez@gmail.com>
2023-12-13 14:21:08 -0300

    criada class Logica com duas opções randômicas

---
+++ scripts/baralla/logica.py
@@ -0,0 +1,23 @@
+from pprint import pprint
+from random import randint
+
+
+class Logica:
+
+    def __init__(self, versao) -> None:
+        self.versao = versao
+        self.VERSOES = {
+            '0.10.0': self.v0_10_0,
+            '0.10.1': self.v0_10_1,
+        }
+
+    def executa(self, mesa, mao):
+        return self.VERSOES[self.versao](mesa, mao)
+
+    def v0_10_0(self, mesa, mao):
+        """Sorteia uma carta"""
+        return randint(0, len(mao)-1)
+
+    def v0_10_1(self, mesa, mao):
+        """Pega a primeira carta recebida"""
+        return 0
