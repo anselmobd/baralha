7768331b3e42d9365ad41c4398e4d51bd457d314
Anselmo Blanco Dominguez <anselmo.blanco.dominguez@gmail.com>
2023-12-13 21:50:50 -0300

    criado baralho espanhol com apenas 2 naipes

--- scripts/baralla/custom/tipo_baralho.py
+++ scripts/baralla/custom/tipo_baralho.py
@@ -5,6 +5,7 @@ class CustomTipoBaralho:
 
     TESTE = 'teste'
     ESPANHOL = 'espanhol'
+    ESPANHOL2N = 'espanhol2n'
     _NAIPE = {
         TESTE:
         {
@@ -15,6 +16,11 @@ class CustomTipoBaralho:
             'o': 'ouros',
             'c': 'copas',
             'e': 'espadas',
+            'p': 'paus',
+        },
+        ESPANHOL2N:
+        {
+            'o': 'ouros',
             'p': 'paus',
         },
     }
@@ -33,6 +39,19 @@ class CustomTipoBaralho:
             '10': '10',
         },
         ESPANHOL:
+        {
+            'a': 'ás',
+            '2': '2',
+            '3': '3',
+            '4': '4',
+            '5': '5',
+            '6': '6',
+            '7': '7',
+            's': 'sota',
+            'c': 'cavalo',
+            'r': 'rei',
+        },
+        ESPANHOL2N:
         {
             'a': 'ás',
             '2': '2',
